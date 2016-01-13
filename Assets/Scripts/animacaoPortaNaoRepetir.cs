3ddf8856169a378762f61ba49f000b95ab76c2ee
paulodgn <paulo.nwork@gmail.com>
2016-01-13 19:00:29 +0000

    Quando jogador abre a jaula o prisioneiro foge.

--- Assets/Scripts/animacaoPortaNaoRepetir.cs
+++ Assets/Scripts/animacaoPortaNaoRepetir.cs
@@ -6,26 +6,36 @@ public class animacaoPortaNaoRepetir : MonoBehaviour {
     Animation anim;
     Animator animator;
     float timer=0;
+    GameObject player;
+    prisionerEscape escape;
 	// Use this for initialization
 	void Start () 
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        escape = GameObject.FindGameObjectWithTag("prisioner").GetComponent<prisionerEscape>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        timer += Time.time;
-        if(timer >= 100)
-        {
-            anim = GetComponent<Animation>();
-            //animator = GetComponent<Animator>();
-            //animator.SetBool("AbrirPorta", true);
-            anim.wrapMode = WrapMode.ClampForever;
-            anim.Play();
-            timer = 0;
-        }
+       
         
         
 	}
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject == player && Input.GetKeyDown(KeyCode.F))
+        {
+            
+                anim = GetComponent<Animation>();
+                anim.wrapMode = WrapMode.ClampForever;
+                anim.Play();
+                escape.startEscape = true;
+                escape.free = true;
+                escape.anim.SetBool("Run", true);
+                
+           
+        }
+    }
 }
--- Assets/Scripts/prisionerEscape.cs
+++ Assets/Scripts/prisionerEscape.cs
@@ -6,23 +6,30 @@ public class prisionerEscape : MonoBehaviour {
     public Transform[] escapePoints;
     int escapePointID;
     NavMeshAgent agent;
-    Animator anim;
-    bool free;
+    public Animator anim;
+    public bool free;
+    public bool startEscape;
 
 	// Use this for initialization
 	void Start () 
     {
-        escapePointID = -1;
+        escapePointID = -2;
         agent = GetComponent<NavMeshAgent>();
         anim = GetComponent<Animator>();
-        free = true;
-        anim.SetBool("Run", true);
-        nextEscapePoint();
+        free = false;
+        startEscape = false;
+        //nextEscapePoint();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+        if (startEscape)
+        {
+            nextEscapePoint();
+            startEscape = false;
+        }
+
 	    if(free)
         {
             if (agent.remainingDistance < 0.5f)
@@ -30,14 +37,19 @@ public class prisionerEscape : MonoBehaviour {
                 nextEscapePoint();
             }
         }
+       
 	}
 
     void nextEscapePoint()
     {
-        if(escapePointID < escapePoints.Length)
-        {
-            escapePointID++;
-        }
-        agent.destination = escapePoints[escapePointID].position;
+        
+            if (escapePointID < escapePoints.Length)
+            {
+                escapePointID++;
+            }
+            agent.destination = escapePoints[escapePointID].position;
+        
     }
+
+
 }
