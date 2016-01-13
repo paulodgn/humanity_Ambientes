41057c8419073486dc667cf16cf8b599e5728794
paulodgn <paulo.nwork@gmail.com>
2016-01-13 17:52:28 +0000

    Prisioneiro a fugir. Ainda está fora da jaula e começa logo a fugir, foi so para testar se o pathfinding esta a funcionar como é suposto.

---
+++ Assets/Scripts/prisionerEscape.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class prisionerEscape : MonoBehaviour {
+
+    public Transform[] escapePoints;
+    int escapePointID;
+    NavMeshAgent agent;
+    Animator anim;
+    bool free;
+
+	// Use this for initialization
+	void Start () 
+    {
+        escapePointID = -1;
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        free = true;
+        anim.SetBool("Run", true);
+        nextEscapePoint();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(free)
+        {
+            if (agent.remainingDistance < 0.5f)
+            {
+                nextEscapePoint();
+            }
+        }
+	}
+
+    void nextEscapePoint()
+    {
+        if(escapePointID < escapePoints.Length)
+        {
+            escapePointID++;
+        }
+        agent.destination = escapePoints[escapePointID].position;
+    }
+}
