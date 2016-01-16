e266b06374e1aa920f6ae1fc733c1df415d35d67
pedroabgmarques <pedroabgmarques@gmail.com>
2016-01-16 17:54:08 +0000

    Possibilidade de saltar o video com o ESC

--- Assets/LogosSlideshow/Scripts/GUI/playMovie.cs
+++ Assets/LogosSlideshow/Scripts/GUI/playMovie.cs
@@ -32,6 +32,10 @@ public class playMovie : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.LoadLevel("GameMenu");
+        }
 	}
+
 }
